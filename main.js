42c952a58fba2c57cece04b5cf0622682e871564
Than2k4 <thanchau.100100@gmail.com>
2024-09-12 23:40:35 +0700

    up

--- main.js
+++ main.js
@@ -24,3 +24,19 @@ prev.addEventListener('click', function (event){
     comment.style.transform = `translateY(${translateY}px)`
     count++
 })
+function toggleForm() {
+    const formPopup = document.getElementById('formPopup');
+    const overlay = document.getElementById('overlay');
+    if (formPopup.style.display === 'block') {
+        formPopup.style.display = 'none';
+        overlay.style.display = 'none';
+    } else {
+        formPopup.style.display = 'block';
+        overlay.style.display = 'block';
+    }
+}
+function scrollToExercise(event) {
+    event.preventDefault()
+    const exerciseSection = document.getElementById('exercise');
+    exerciseSection.scrollIntoView({ behavior: 'smooth' })
+}
